8972906d0816240b10d4c4dda3d3c7dcdadbdb6a
Maxim <maksim.krugley@bk.ru>
2024-04-02 22:45:56 +0300

    vendor control fix

--- SalutemCRM/ViewModels/SearchVendorControlViewModel.cs
+++ SalutemCRM/ViewModels/SearchVendorControlViewModel.cs
@@ -20,7 +20,7 @@ namespace SalutemCRM.ViewModels;
 
 public partial class SearchVendorControlViewModelContext : ObservableObject
 {
-    public bool IsResponsiveSelected => SelectedVendor != null;
+    public bool IsVendorSelected => SelectedVendor != null;
 
     [ObservableProperty]
     private bool _isResponsiveControl = false;
@@ -35,7 +35,7 @@ public partial class SearchVendorControlViewModelContext : ObservableObject
     [NotifyPropertyChangedFor(nameof(IsPage1))]
     [NotifyPropertyChangedFor(nameof(IsPage2))]
     [NotifyPropertyChangedFor(nameof(IsPage3))]
-    private int _activePage = 1;
+    private int _activePage = 0;
 
     public bool IsPage1 => ActivePage == 0;
     public bool IsPage2 => ActivePage == 1;
@@ -45,7 +45,7 @@ public partial class SearchVendorControlViewModelContext : ObservableObject
     private string _searchVendorInputStr = "";
 
     [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(IsResponsiveSelected))]
+    [NotifyPropertyChangedFor(nameof(IsVendorSelected))]
     private Vendor? _selectedVendor = null;
 
     [ObservableProperty]
@@ -84,18 +84,33 @@ public class SearchVendorControlViewModel : ViewModelBase
     public ReactiveCommand<Unit, Unit> GoAddVendor { get; }
     public ReactiveCommand<Vendor, Unit> GoEditVendor { get; }
     public ReactiveCommand<Unit, Unit> AddNewVendor { get; }
+    public ReactiveCommand<Unit, Unit> EditVendor { get; }
 
     public SearchVendorControlViewModel()
     {
         Context.Notify += SearchVendorByInput;
 
-        IObservable<bool> isInputValid = this.WhenAnyValue(
-            x => x.Context.NewVendor,
-            (vendor) =>
-                vendor != null &&
-                (vendor.Name ?? "").Length > 5 &&
-                (vendor.Address ?? "").Length > 5
-        ).DistinctUntilChanged();
+        IObservable<bool> _ifNewVendorFilled = this.WhenAnyValue(
+            x => x.Context.NewVendor, x => x.Context.NewVendor.Name, x => x.Context.NewVendor.Address,
+            (obj, n, a) =>
+                obj != null &&
+                !string.IsNullOrWhiteSpace(n) &&
+                !string.IsNullOrWhiteSpace(a) &&
+                n.Length >= 5 &&
+                a.Length >= 5
+        );
+
+        IObservable<bool> _ifEditVendorFilled = this.WhenAnyValue(
+            x => x.Context.EditVendor.Name, x => x.Context.EditVendor.Address, x => x.Context.EditTempVendor.Name, x => x.Context.EditTempVendor.Address,
+            (on, oa, nn, na) =>
+                !string.IsNullOrWhiteSpace(on) &&
+                !string.IsNullOrWhiteSpace(oa) &&
+                !string.IsNullOrWhiteSpace(nn) &&
+                !string.IsNullOrWhiteSpace(na) &&
+                nn.Length >= 5 &&
+                na.Length >= 5 &&
+                (on != nn || oa != na)
+        );
 
         GoBack = ReactiveCommand.Create(() => {
             Context.ActivePage = 0;
@@ -110,19 +125,42 @@ public class SearchVendorControlViewModel : ViewModelBase
             Context.ActivePage = 2;
             Context.EditVendor = x.Clone() as Vendor;
             Context.EditTempVendor = x.Clone() as Vendor;
+            Context.EditTempVendor!.AdditionalInfo = "";
         });
 
         AddNewVendor = ReactiveCommand.Create(() => {
-            using (DatabaseContext db = new DatabaseContext(DatabaseContext.ConnectionInit()))
-            {
-                db.Vendors.Add(Context.NewVendor!);
-                db.SaveChanges();
-            }
+            Context
+            .DoIf(x => {
+                using (DatabaseContext db = new DatabaseContext(DatabaseContext.ConnectionInit()))
+                {
+                    db.Vendors.Add(x.NewVendor!);
+                    db.SaveChanges();
+                };
+            }, x => x.NewVendor != null)?
+            .DoInst(x => x.SearchVendorInputStr = x.NewVendor!.Name)
+            .DoInst(x => x.NewVendor = new())
+            .DoInst(x => x.ActivePage = 0);
+        }, _ifNewVendorFilled);
 
-            Context.SearchVendorInputStr = Context.NewVendor!.Name;
-            Context.NewVendor = new();
-            GoBack.Execute();
-        }, isInputValid);
+        EditVendor = ReactiveCommand.Create(() => {
+            Context
+            .Do(x => {
+                using (DatabaseContext db = new DatabaseContext(DatabaseContext.ConnectionInit()))
+                {
+                    Vendor? vendor = db.Vendors.Single(v => v.Name == x.EditVendor!.Name);
+                    vendor.Name = x.EditTempVendor!.Name;
+                    vendor.Address = x.EditTempVendor!.Address;
+                    vendor.AdditionalInfo +=
+                        $"\n\n{DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss")}, \n...пользователь..." +
+                        $"\n{((x.EditTempVendor!.AdditionalInfo ?? "").Length > 0 ? x.EditTempVendor!.AdditionalInfo : "*тихо внесена правка*")}";
+
+                    db.SaveChanges();
+                };
+            })?
+            .DoInst(x => x.SearchVendorInputStr = x.EditTempVendor!.Name)
+            .DoInst(x => x.ActivePage = 0);
+        }, _ifEditVendorFilled);
+
 
         if (!Design.IsDesignMode)
             SearchVendorByInput("");
