9a79077a7bd4b5e36389568a50a222b37135f6f9
Maxim <maksim.krugley@bk.ru>
2024-05-22 16:45:43 +0300

    test solutions for external (ethernet) connections

---
+++ Test.DockerDBConnection/Program.cs
@@ -0,0 +1,33 @@
+﻿using SalutemCRM;
+using SalutemCRM.Database;
+
+namespace Test.DockerDBConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Docker DB connection");
+
+            try
+            {
+                using (DatabaseContext db = new(DatabaseContext.ConnectionInit()))
+                {
+                    var x = db.CurrencyUnits.Where(s => s.Name.Length > 0);
+
+                    Console.WriteLine("Currency list:");
+                    x.DoForEach(s => Console.WriteLine(s.Name));
+                }
+                 
+                Console.WriteLine($"Connected to Docker Database successfully!");
+            }
+            catch
+            {
+                Console.WriteLine($"Connection troubles...");
+            }
+
+            Console.WriteLine("Press any key.");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Test.TCPConnection/Program.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SalutemCRM.TCP;
+
+namespace Test.TCPConnection;
+
+internal class Program
+{
+    public static IHost? Host { get; private set; }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("TCP server connection");
+
+        Host =
+            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder()
+            .ConfigureServices(services => {
+                services.AddSingleton<TCPChannel>();
+            })
+            .Build();
+
+        try
+        {
+            Host!.Services.GetService<TCPChannel>()!.Open();
+            Console.WriteLine($"Connected to {Host!.Services.GetService<TCPChannel>()!.thisServer.IpAddress}:{Host!.Services.GetService<TCPChannel>()!.thisServer.Port}!");
+        }
+        catch
+        {
+            Console.WriteLine($"Connection troubles...");
+        }
+
+        Console.WriteLine("Press any key.");
+        Console.ReadKey();
+    }
+}
