c8a17e262c273b079832a05dc7b9c26d8963f2c8
Maxim <maksim.krugley@bk.ru>
2024-05-23 13:53:22 +0300

    order products manufacture preperation control part 3

--- SalutemCRM/ViewModels/OrderManufactureControlViewModel.cs
+++ SalutemCRM/ViewModels/OrderManufactureControlViewModel.cs
@@ -26,18 +26,27 @@ public partial class OrderManufactureControlViewModelSource : ReactiveControlSou
     private Manufacture? _selectedProduct;
 
     [ObservableProperty]
+    private MaterialFlow? _materialInRotation;
+
+    [ObservableProperty]
     private bool _isOverlayAddTask = false;
 
     [ObservableProperty]
     private bool _isOverlayAddMaterial = false;
 
+    [ObservableProperty]
+    private bool _isOverlayMaterialRotation = false;
+
+    [ObservableProperty]
+    private bool _isMaterialTakenFromTemplate = true;
+
     public OrderManufactureControlViewModelSource() => SelectedItemChangedTrigger += _ =>
     {
-        IsOverlayAddMaterial = IsOverlayAddTask = false;
+        HideAllOverlays();
+        SelectedProduct = null;
+        OrderOnPrep = null;
 
-        if (SelectedItem == null)
-            OrderOnPrep = null;
-        else
+        if (SelectedItem != null)
             using (DatabaseContext db = new(DatabaseContext.ConnectionInit()))
                 OrderOnPrep = db.Orders
                 .Where(x => x.Id == SelectedItem!.Id)
@@ -48,26 +57,92 @@ public partial class OrderManufactureControlViewModelSource : ReactiveControlSou
                     .ThenInclude(x => x.OrderProcesses)
                 .First();
     };
+
+    public void HideAllOverlays() => false
+        .Do(x => IsOverlayAddMaterial = x)
+        .Do(x => IsOverlayAddTask = x)
+        .Do(x => IsOverlayMaterialRotation = x);
+
+    public void RemoveMaterialFromBuilder(MaterialFlow _remove) => SelectedProduct?.Do(x => x.MaterialFlows.Remove(_remove));
+
+    public void RunMaterialRotation(MaterialFlow _rotation)
+    {
+        IsOverlayMaterialRotation = true;
+        MaterialInRotation = _rotation;
+    }
+
+    public void AddMaterialsIntoProduction()
+    {
+        if (IsMaterialTakenFromTemplate && CRUSProductTemplateControlViewModelSource.GlobalContainer.SelectedItem is not null)
+        {
+            using (DatabaseContext db = new(DatabaseContext.ConnectionInit()))
+                db.ProductTemplates
+                    .Where(x => x.Id == CRUSProductTemplateControlViewModelSource.GlobalContainer.SelectedItem.Id)
+                    .Include(x => x.ProductSchemas)
+                        .ThenInclude(x => x.WarehouseItem)
+                            .ThenInclude(x => x!.Category)
+                    .ToList()
+                    .SelectMany(x => x.ProductSchemas)
+                    .Select(x => x.WarehouseItem)
+                    .DoIf(x => { }, x => x.Count() > 0)?
+                    .DoForEach(x =>
+                    {
+                        if (SelectedProduct!.MaterialFlows!.SingleOrDefault(s => s.WarehouseItem!.Id == x!.Id) is MaterialFlow match && match is not null)
+                            ++match.CountReservedFromStock;
+                        else
+                            SelectedProduct!.MaterialFlows!.Add(new()
+                            {
+                                CountReservedFromStock = 1,
+                                WarehouseItem = x,
+                                WarehouseItemForeignKey = x!.Id
+                            });
+                    });
+
+            HideAllOverlays();
+        }
+        else if (!IsMaterialTakenFromTemplate)
+        {
+        }
+    }
 }
 
 public class OrderManufactureControlViewModel : ViewModelBase<Order, OrderManufactureControlViewModelSource>
 {
+    public IObservable<bool>? IsProductSelected { get; protected set; }
+
     public ReactiveCommand<Unit, Unit>? ShowOverlayAddTaskCommand { get; protected set; }
+    
     public ReactiveCommand<Unit, Unit>? ShowOverlayAddMaterialCommand { get; protected set; }
+    
     public ReactiveCommand<Unit, Unit>? HideOverlaysCommand { get; protected set; }
+
+    public ReactiveCommand<MaterialFlow, Unit>? RemoveMaterialFromBuilderCommand { get; protected set; }
+
+    public ReactiveCommand<MaterialFlow, Unit>? RunMaterialRotationCommand { get; protected set; }
+
+    public ReactiveCommand<Unit, Unit>? AddMaterialsIntoProductionCommand { get; protected set; }
 
     public OrderManufactureControlViewModel() : base(new() { PagesCount = 1 })
     {
+        IsProductSelected = this.WhenAnyValue(
+            x => x.Source.SelectedProduct,
+            x => x.Source.SelectedProduct,
+            (item1, item2) =>
+                item1 is not null
+        );
+
         GoBackCommand = ReactiveCommand.Create(NavigationViewModelSource.SetRegisteredWindowContent<OrdersManagmentControl>);
 
-        ShowOverlayAddTaskCommand = ReactiveCommand.Create(() => { Source.IsOverlayAddTask = true; });
+        ShowOverlayAddTaskCommand = ReactiveCommand.Create(() => { Source.IsOverlayAddTask = true; }, IsProductSelected);
 
-        ShowOverlayAddMaterialCommand = ReactiveCommand.Create(() => { Source.IsOverlayAddMaterial = true; });
+        ShowOverlayAddMaterialCommand = ReactiveCommand.Create(() => { Source.IsOverlayAddMaterial = true; }, IsProductSelected);
 
-        HideOverlaysCommand = ReactiveCommand.Create(() =>
-        {
-            Source.IsOverlayAddTask = false;
-            Source.IsOverlayAddMaterial = false;
-        });
+        HideOverlaysCommand = ReactiveCommand.Create(Source.HideAllOverlays);
+
+        RemoveMaterialFromBuilderCommand = ReactiveCommand.Create<MaterialFlow>(Source.RemoveMaterialFromBuilder);
+
+        RunMaterialRotationCommand = ReactiveCommand.Create<MaterialFlow>(Source.RunMaterialRotation);
+
+        AddMaterialsIntoProductionCommand = ReactiveCommand.Create(Source.AddMaterialsIntoProduction);
     }
 }
